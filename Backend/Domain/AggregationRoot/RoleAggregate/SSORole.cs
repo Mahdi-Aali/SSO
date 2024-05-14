c4ccd9ddafb3fc72cf7ab115dc8c83182b496824
mahdi <mahdi1383harold@outlook.com>
2024-05-15 00:49:43 +0330

    backend-service-initialized

---
+++ Backend/Domain/AggregationRoot/RoleAggregate/SSORole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.AggregationRoot.RoleAggregate;
+
+public class SSORole : IdentityRole<Guid>
+{
+
+}
---
+++ Backend/Infrastructure/InfrastructureDependencyProvider.cs
@@ -0,0 +1,47 @@
+﻿using Core.DependencyInjection;
+using Domain.AggregationRoot.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Infrastructure;
+
+public sealed class InfrastructureDependencyProvider : IDependencyInjectionProvider
+{
+    public static IServiceCollection GetAssemblyDependencies(IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services, configuration);
+        AddIdentityCore(services);
+
+        return services;
+    }
+
+
+    public static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SSODatabaseContext>(opt =>
+        {
+            opt.UseSqlServer(configuration.GetConnectionString("Default"), sqlServerOptions =>
+            {
+                sqlServerOptions
+                .CommandTimeout(TimeSpan.FromSeconds(3).Seconds)
+                .EnableRetryOnFailure(5, TimeSpan.FromSeconds(5), null)
+                .MigrationsAssembly(typeof(InfrastructureDependencyProvider).Assembly.GetName().Name);
+            });
+        });
+    }
+
+
+
+    public static void AddIdentityCore(IServiceCollection services)
+    {
+        services.AddIdentityCore<SSOUser>(cfg =>
+        {
+            cfg.User.RequireUniqueEmail = true;
+            cfg.SignIn.RequireConfirmedEmail = true;
+            cfg.Stores.ProtectPersonalData = true;
+        })
+            .AddEntityFrameworkStores<SSODatabaseContext>();
+    }
+}
